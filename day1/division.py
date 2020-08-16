908c8e86513fb4234d8a334f5d35c186061b2b7d
adwaith2000 <44024519+adwaith2000@users.noreply.github.com>
2020-08-16 20:40:25 +0530

    Update division.py

--- day1/division.py
+++ day1/division.py
@@ -1,6 +1,6 @@
 '''Read two integers and print two lines. The first line should contain integer
  division,a //b . The second line should contain float division, a/b
-
+qstn 1
 You don't need to perform any rounding or formatting operations. '''
 if __name__ == '__main__':
     a = int(input())
